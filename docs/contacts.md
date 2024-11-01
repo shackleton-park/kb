30bc26b2e1e6001fdf87ae907ec27caa48c41616
Romulo Ferreira <romulo.nf@gmail.com>
2024-11-01 11:21:58 +0000

    chore: initial scaffold

---
+++ docs/contacts.md
@@ -0,0 +1,19 @@
+---
+sidebar_position: 3
+---
+
+# Contacts
+
+This page contains a few useful contacts:
+
+## Residents committee
+
+- Email: shackletonresidents@gmail.com
+
+## Petra
+
+Petra is the company responsible for the management of the estate.
+
+- Email: megan@petra.ie
+- Phone: +353 12935103
+- Website: https://petra.ie/
---
+++ docusaurus.config.ts
@@ -0,0 +1,112 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Shackleton Park',
+  tagline: 'Knowledge Base',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://shackleton-park.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/kb',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Shackleton Park',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Knowledge base',
+        },
+        {
+          href: 'https://github.com/shackleton-park/kb',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Knowledge base',
+              to: '/docs',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [],
+        },
+        {
+          title: 'Links',
+          items: [
+            {
+              label: 'Petra',
+              href: 'https://petra.ie/',
+            },
+            {
+              label: 'Cairn Homes',
+              href: 'https://www.cairnhomes.com/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()}`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
